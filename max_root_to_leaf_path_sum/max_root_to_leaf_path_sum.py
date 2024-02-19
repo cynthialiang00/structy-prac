eb77b664d44753a2decb9f62521075659d7ca206
Cynthia Liang <118590381+cynthialiang00@users.noreply.github.com>
2024-02-19 15:11:33 -0800

    Solved 'max root to leaf path sum' (.py)

--- max_root_to_leaf_path_sum/max_root_to_leaf_path_sum.py
+++ max_root_to_leaf_path_sum/max_root_to_leaf_path_sum.py
@@ -1,18 +1,21 @@
-# class Node:
-#   def __init__(self, val):
-#     self.val = val
-#     self.left = None
-#     self.right = None
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+    
 import math
 ​
 def max_path_sum(root):
   
   def traverse_sum(root):
+    
     if not root:
       return -math.inf
+    print(f'node: {root.val}')
     if not root.left and not root.right:
       return root.val
-    print(f'node: {root.val}')
+    
     left_sum = traverse_sum(root.left)
     right_sum = traverse_sum(root.right)
     
@@ -23,11 +26,13 @@ def max_path_sum(root):
     
 ​
     
-    
-    
-    
-    
-      
-  
-  
+a = Node(3)
+b = Node(11)
+c = Node(4)
+d = Node(4)
+e = Node(-2)
+f = Node(1)
 ​
+a.left = b
+a.right = c
+    
